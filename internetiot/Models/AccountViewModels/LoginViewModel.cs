691b60b97de070439ce482e61136d5358d1df37c
Roy Koren <roykoren10@gmail.com>
2021-08-03 03:18:46 +0300

    Added LoginViewModel file

---
+++ internetiot/Models/AccountViewModels/LoginViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+namespace internetiot.Models.AccountViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe
+        {
+            get; set;
+        }
+    }
+}
