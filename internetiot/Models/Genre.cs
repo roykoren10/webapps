4db634bc80aa9af74daa4e6a2f32d029a5abd29e
meyron1456 <meyron1456@gmail.com>
2021-08-06 16:34:25 +0300

    adding Genre model

---
+++ internetiot/Models/Genre.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace internetiot.Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+
+        [Display(Name ="Genre Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Display(Name="Minimum Age")]
+        public int MinAge { get; set; } //incase we want to add minimum age for certain Rage rooms
+    }
+}
