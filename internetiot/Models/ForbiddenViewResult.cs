e9f0efc58314780a44cf48067eb7d744f8a05f72
meyron1456 <meyron1456@gmail.com>
2021-08-10 16:50:20 +0300

    adding Forbidden View Result model

---
+++ internetiot/Models/ForbiddenViewResult.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace internetiot.Models
+{
+    public class ForbiddenViewResult : ViewResult
+    {
+        //public string ViewName { get; set; }
+        //public int StatusCode { get; set; }
+        public ForbiddenViewResult(string viewName, string message = "Forbidden")
+        {
+            this.ViewName = viewName;
+            this.StatusCode = (int)HttpStatusCode.Forbidden;
+            this.ViewData = new ViewDataDictionary(new Microsoft.AspNetCore.Mvc.ModelBinding.EmptyModelMetadataProvider(), new ModelStateDictionary());
+            ViewData["Message"] = message;
+        }
+    }
+}
