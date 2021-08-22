1a5915b0ac207bdbd4c7548f1462d8388cd73013
meyron1456 <meyron1456@gmail.com>
2021-08-22 03:10:24 +0300

    adding Data folder

---
+++ internetiot/Data/RageRoomsContext.cs
@@ -0,0 +1,32 @@
+﻿using internetiot.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace internetiot.Data
+{
+    public class RageRoomsContext:IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<RageRoom> RageRooms { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Genre> Genre { get; set; }
+
+        public RageRoomsContext(DbContextOptions<RageRoomsContext> options) 
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+       
+    }
+}
